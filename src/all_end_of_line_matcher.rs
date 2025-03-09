aec4648d99efa0b17124e3bc0dffcc9b4c21e1c5
Florent GABRIEL <hovjyc@nyol.fr>
2025-03-09 18:08:50 +0000

    Create all_end_of_line_matcher.rs

---
+++ src/all_end_of_line_matcher.rs
@@ -0,0 +1,43 @@
+use crate::match_result::MatchResult;
+use crate::matcher::Matcher;
+
+struct AllEndOfLineMatcher {
+    prev_char: u8,
+}
+impl AllEndOfLineMatcher {
+    pub fn new() -> Self {
+        Self { prev_char: 0 }
+    }
+}
+impl Matcher for AllEndOfLineMatcher {
+    // This function is called at each byte read
+    //   `el_buf` contains the value of the byte
+    //   `pos` contains the position matched
+    fn sequel(&mut self, el_buf: u8, pos: usize) -> MatchResult {
+        if pos == 0 {
+            if el_buf == b'\r' || el_buf == b'\n' {
+                self.prev_char = el_buf;
+                MatchResult::NeedNext
+            } else {
+                MatchResult::Mismatch
+            }
+        } else if pos == 1 {
+            if el_buf == b'\n' && self.prev_char == b'\r' {
+                MatchResult::Match(0, 0) //We are on \r\n
+            } else {
+                MatchResult::Match(0, 1) //We have to ignore the last byte since it's not a part of the end of line pattern
+            }
+        } else {
+            panic!("We can't reach this code since we just manage 2 positions")
+        }
+    }
+
+    // This function is called at the end of the buffer, useful to manage partial cases
+    fn sequel_eos(&mut self, pos: usize) -> MatchResult {
+        if pos == 0 {
+            MatchResult::Match(0, 0) //Here the last char is \r or \n, at position 0
+        } else {
+            panic!("We can't reach this code since we just manage 2 positions")
+        }
+    }
+}
